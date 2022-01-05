29d17c958157cc451c928f0bb1ab768636f5d7ba
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-01-05 13:17:47 -0600

    Add file reading to parser
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- nfdocs-parser.py
+++ nfdocs-parser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
 
-nextflow_file = sys.argv[1]
-print(nextflow_file)
+nextflow_path = sys.argv[1]
+with open(nextflow_path) as nextflow_file:
+    print(nextflow_file)
