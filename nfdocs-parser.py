fc4c536ab60a8d892a93410ce6b7f82aa413a1bb
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-01-05 13:19:33 -0600

    Add line extraction to parser
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- nfdocs-parser.py
+++ nfdocs-parser.py
@@ -3,4 +3,6 @@ import sys
 
 nextflow_path = sys.argv[1]
 with open(nextflow_path) as nextflow_file:
-    print(nextflow_file)
+    nextflow_lines = nextflow_file.readlines()
+    for i in range(1, 10):
+        print(nextflow_lines[i])
