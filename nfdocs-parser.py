4f6fc88e40cdd0c8e251e6a43ff5942e40a00d9a
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-01-05 13:20:35 -0600

    Add comments to parser
    Signed-off-by: Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>

--- nfdocs-parser.py
+++ nfdocs-parser.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 import sys
 
+# Take path as single argument for now
 nextflow_path = sys.argv[1]
 with open(nextflow_path) as nextflow_file:
+
+    # Split by lines
     nextflow_lines = nextflow_file.readlines()
+
+    # Print the first few lines
     for i in range(1, 10):
         print(nextflow_lines[i])
